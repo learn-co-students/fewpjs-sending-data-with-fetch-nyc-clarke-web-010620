99126ec4fa7bd028493f7c13d504d91d29012dae
mdiaz26 <mike.diaz006@gmail.com>
2020-03-03 15:53:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(username, useremail){
+    let objectBody =
+        {
+            name: username,
+            email: useremail
+        }
+    configObj.body = JSON.stringify(objectBody)
+    return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(json => append(json))
+    .catch(function(error) {
+        alert("There has been an error");
+        let div = document.createElement('div')
+        div.innerHTML = `${error}`
+        document.body.append(div)})
+}
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    }
+}
+
+function append(json){
+    let div = document.createElement('div')
+    div.innerHTML = `
+    <p>username: ${json.name}</p>
+    <p>user email: ${json.email}</p>
+    `
+    document.body.append(div)
+}
